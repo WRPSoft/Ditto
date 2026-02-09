4a6322ba0eb4c9f19101db832bf4f36fd0fb88e9
WRPSoft <78589032+WRPSoft@users.noreply.github.com>
2026-02-09 10:44:06 +0100

    Update DittoChaiScript.h
    Added ChaiScript caller for external apps via ShellExecute command

--- src/DittoChaiScript.h
+++ src/DittoChaiScript.h
@@ -35,6 +35,13 @@ public:
 	BOOL DescriptionMatchesRegex(std::string regex);
 	void DescriptionReplaceRegex(std::string regex, std::string replaceWith);
 
+	// RW: 2026-01-28 17:06:27 added ChaiScript caller for external apps via ShellExecuteA command
+	void CallExternal(std::string command, std::string params, bool dobeep, bool debugmode);
+	// RW: 2026-01-28 17:06:27 added ChaiScript for easier getting file extension
+	std::string CDittoChaiScript::GetFileExtension(const std::string FileName);
+	// RW: 2026-02-03 15:09:42 copied picture files can be normally edited
+	std::string CDittoChaiScript::GetPictureFileName();
+
 	void SetMakeTopSticky();
 	void SetMakeLastSticky();
 	void SetReplaceTopSticky();
